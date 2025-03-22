78d0f2f647a56912c45c6081b9dccf4f91598859
Pablo Paulino <pablopaulinodev@gmail.com>
2025-03-22 17:57:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function redirecionarWhatsApp(numero = '17997853265', mensagem = 'Confirmo a presença no aniversário!') {
+function redirecionarWhatsApp(numero = '17997853265', mensagem = 'Confirmo a presença no aniversário da Maria Heloísa!') {
     const linkWhatsApp = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
     window.open(linkWhatsApp, "_blank");
     }
